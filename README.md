41b452e69a9fa6310b9fe24881057324d858b3c4
ryanqgle <ryanqle24@gmail.com>
2026-05-05 13:18:57 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
+Ryan Le
+
+## 1. 
+
+Q: Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+A: Yes, because you need to test if the message is even created. Once you ensure that the message is created, you still need to further test the feature and its functionality, like if it sends/receives correctly, is the message too big/empty, and edge cases. 
+
+## 2. 
+
+Q: Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+A: Yes, I would still unit test the max message length feature because there may be varying formats/languages. Some inputs may include characters that consume multiple or an incorrect amount of characters in the message. If the message contains too much data, which is what I'm guessing the threshold is to prevent, it will slow down the system. Moreover, we must also test edge cases, like: does it count 80 characters correctly, does it validate 79 characters, or 81, or empty. 
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.querySelector('#horn-select');
+  const volumeControl = document.querySelector('#volume');
+  const playButton = document.querySelector('button');
+  const hornImage = document.querySelector('img');
+  const audioElement = document.querySelector('audio');
+
+  let horn = [];
+
+  hornSelect.addEventListener('change', () => {
+    const selectedHorn = hornSelect.value;
+  
+    if (selectedHorn === 'air-horn') {
+      hornImage.src = 'assets/images/air-horn.svg';
+      audioElement.src = 'assets/audio/air-horn.mp3';
+    } else if (selectedHorn === 'car-horn') {
+      hornImage.src = 'assets/images/car-horn.svg';
+      audioElement.src = 'assets/audio/car-horn.mp3';
+    } else if (selectedHorn === 'party-horn') {
+      hornImage.src = 'assets/images/party-horn.svg';
+      audioElement.src = 'assets/audio/party-horn.mp3';
+    } else {
+      hornImage.src = 'assets/images/no-image.png';
+      audioElement.src = '';
+    }
+  });
+  
+  volumeControl.addEventListener('input', () => {
+    const volumeValue = volumeControl.value;
+    audioElement.volume = volumeValue / 100; /* in html, min = 0, max = 100 */
+    if (volumeValue == 0) {
+      document.querySelector('#volume-controls img').src = 'assets/icons/volume-level-0.svg';
+    } else if (volumeValue >= 1 && volumeValue < 33) {
+      document.querySelector('#volume-controls img').src = 'assets/icons/volume-level-1.svg'; 
+    } else if (volumeValue >= 33 && volumeValue < 67) {
+      document.querySelector('#volume-controls img').src = 'assets/icons/volume-level-2.svg';
+    } else {
+      document.querySelector('#volume-controls img').src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  playButton.addEventListener('click', () => {
+    audioElement.play();
+    if (hornSelect.value === 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
 }
